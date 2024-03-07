70fa3e03a1a9c9bb8e2a91ed8c0ab0763ff7b566
Ali İhsan Turan <aliihsan741@hotmail.com>
2024-03-07 13:44:49 +0300

    cartpage created

--- app/src/main/java/com/example/foodapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/ui/MainActivity.kt
@@ -2,11 +2,47 @@ package com.example.foodapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.foodapp.R
+import com.example.foodapp.databinding.ActivityMainBinding
+import com.example.foodapp.ui.fragment.CartFragment
+import com.example.foodapp.ui.fragment.FavoriteFragment
+import com.example.foodapp.ui.fragment.HomeFragment
+import com.example.foodapp.ui.fragment.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.favorite -> {
+                    loadFragment(FavoriteFragment())
+                    true
+                }
+                R.id.profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                R.id.cart -> {
+                    loadFragment(CartFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainerView,fragment)
+        transaction.commit()
     }
 }
